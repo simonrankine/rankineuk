73b2ade4201f918f1947a7c4ab50e8858aaed709
Simon Rankine <simon@rankine.me>
2021-01-18 21:41:10 +0000

    Install Cloud-Watch agent

--- codedeploy_scripts/ansible.sh
+++ codedeploy_scripts/ansible.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ansible-galaxy collection install community.general
+ansible-galaxy collection install community.general 
+ansible-galaxy install christiangda.amazon_cloudwatch_agent
 ansible-playbook /tmp/ansible/playbook.yml
